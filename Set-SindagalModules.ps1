88f54b880ea626a95ca4f05efcb52b23699149f0
denzii <denizarca1@gmail.com>
2021-10-09 23:17:48 +0300

    entry script bugfixes

--- Set-SindagalModules.ps1
+++ Set-SindagalModules.ps1
@@ -1,11 +1,14 @@
 $userPsModulesDir = ($env:PsModulePath -split '(?=[;])',5)[0]
-$singadalModulesDir = "$userPsModulesDir/Sindagal"
+$singadalModuleDir = "$userPsModulesDir\Sindagal"
 
-If(!(test-path $singadalModulesDir))
+If(!(test-path $singadalModuleDir))
 {
     New-Item -Path $singadalModulesDir -ItemType Directory
 }
 
-Invoke-WebRequest -uri  "https://raw.githubusercontent.com/denzii/sindagal/master/Sindagal.psm1" -Method "GET" -Outfile $singadalModulesDir
+$sindagalModulePath = $singadalModuleDir + "\Sindagal.psm1"
+Invoke-WebRequest -uri  "https://raw.githubusercontent.com/denzii/sindagal/master/Sindagal.psm1" -Method "GET" -Outfile $sindagalModulePath
 
 Import-Module -Global -Name Sindagal
+
+Get-Module -ListAvailable | Where-Object Name -eq "Sindagal"
